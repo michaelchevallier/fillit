50becd1accda264b1f40f8d55b163db416429da2
Michael CHEVALLIER <mchevall@e2r4p16.42.fr>
2016-01-19 17:49:58 +0100

    oops

--- solution_cpy.c
+++ solution_cpy.c
@@ -6,7 +6,7 @@
 /*   By: mchevall <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/19 16:25:52 by mchevall          #+#    #+#             */
-/*   Updated: 2016/01/19 17:35:07 by mchevall         ###   ########.fr       */
+/*   Updated: 2016/01/19 17:49:43 by mchevall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,34 +35,4 @@ char	**solution_cpy(t_map *map)
 		i++;
 	}
 	return(solution);
-}
-
-#include <stdio.h>
-
-int main()
-{
-	t_map *map;
-	int i;
-
-	map->c_range = 5;
-	map->nb_tetri = 7;
-	i = 0;
-	map->solution = NULL;
-	map->map = (char **)ft_memalloc(sizeof(char *) * map->c_range + 1);
-	map->map[map->c_range] = NULL;
-
-	while (i < map->c_range)
-	{
-		map->map[i] = ft_strdup("a");
-		i++;
-	}
-	i = 0;
-	while (i < map->c_range)
-	{
-		map->solution = solution_cpy(map);
-		printf("%s\n", map->solution[i]);
-		i++;
-	}
-	range_min(map);
-	printf("%d\n", map->m_range);
 }
