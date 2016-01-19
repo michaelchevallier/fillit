e73955d934c77e9edb201aecc2c15c02f667072f
Michael CHEVALLIER <mchevall@e2r4p16.42.fr>
2016-01-19 17:54:05 +0100

    toto

--- main.c
+++ main.c
@@ -5,20 +5,38 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mchevall <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/12/18 15:51:39 by mchevall          #+#    #+#             */
-/*   Updated: 2016/01/12 17:47:08 by mchevall         ###   ########.fr       */
+/*   Created: 2016/01/19 17:37:48 by mchevall          #+#    #+#             */
+/*   Updated: 2016/01/19 17:38:22 by mchevall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "fillit.h"
 
-int		main(int argc, char **argv)
+int main()
 {
-	if (argc != 2)
+	t_map *map;
+	int i;
+
+	map->c_range = 5;
+	map->nb_tetri = 7;
+	i = 0;
+	map->solution = NULL;
+	map->map = (char **)ft_memalloc(sizeof(char *) * map->c_range + 1);
+	map->map[map->c_range] = NULL;
+
+	while (i < map->c_range)
 	{
-		ft_putstr("Please input one and only one argument !!\n");
-		return (1);
+		map->map[i] = ft_strdup("a");
+		i++;
 	}
-	read_map(argv[1]);
-	return (0);
+	i = 0;
+	while (i < map->c_range)
+	{
+		map->solution = solution_cpy(map);
+		printf("%s\n", map->solution[i]);
+		i++;
+	}
+	range_min(map);
+	printf("%d\n", map->m_range);
 }
