36fb05c2326cde53929624472f571867e29c552b
Michael CHEVALLIER <mchevall@e2r7p21.42.fr>
2016-01-26 13:54:34 +0100

    bob

--- pos.c
+++ pos.c
@@ -6,60 +6,31 @@
 /*   By: dgalide <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/25 17:15:48 by dgalide           #+#    #+#             */
-/*   Updated: 2016/01/25 18:50:37 by dgalide          ###   ########.fr       */
+/*   Updated: 2016/01/25 19:14:04 by mchevall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int			update_pos(t_map *map)
-{
-	int		x;
-	int		y;
-
-	x = map->c_pos[0];
-	y = map->c_pos[1] + 1;
-	while (x < map->c_range)
-	{
-		while (y < map->c_range)
-		{
-			if (map->map[x][y] == '.')
-			{
-				map->c_pos[0] = x;
-				map->c_pos[1] = y;
-				return (1);
-			}
-			y++;
-		}
-		y = 0;
-		x++;
-	}
-	return (0);
-}
-
 int			check_space(t_map *map, int *lst, int x, int y)
 {
 	int	a;
 	int	b;
-	int	l;
+	int	i;
+	int l;
 
-	a = lst[0];
-	b = lst[1];
+	i = 0;
 	l = 0;
-	if (((((x + a) < map->c_range) && ((x + a) > -1)) && ((((y + b) < map->c_range)) && ((y + b) > -1))) && map->map[(x + a)][(y + b)] == '.')
-		l++;
-	a = lst[2];
-	b = lst[3];
-	if (((((x + a) < map->c_range) && ((x + a) > -1)) && ((((y + b) < map->c_range)) && ((y + b) > -1))) && map->map[(x + a)][(y + b)] == '.')
-		l++;
-	a = lst[4];
-	b = lst[5];
-	if (((((x + a) < map->c_range) && ((x + a) > -1)) && ((((y + b) < map->c_range)) && ((y + b) > -1))) && map->map[(x + a)][(y + b)] == '.')
-		l++;
-	a = lst[6];
-	b = lst[7];
-	if (((((x + a) < map->c_range) && ((x + a) > -1)) && ((((y + b) < map->c_range)) && ((y + b) > -1))) && map->map[(x + a)][(y + b)] == '.')
-		l++;
+	while (i < 8)
+	{
+		a = lst[i];
+		b = lst[i + 1];
+		if (((((x + a) < map->c_range) && ((x + a) > -1)) &&
+					((((y + b) < map->c_range)) &&
+					((y + b) > -1))) && map->map[(x + a)][(y + b)] == '.')
+			l++;
+		i += 2;
+	}
 	return ((l == 4) ? 1 : 0);
 }
 
